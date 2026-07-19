f4b3f65376b5ce9c68bd254cc9d5d7cc37ab6831
Rolando Valle <rolandovalle0428@gmail.com>
2026-07-19 20:36:53 +0800

    updatea

--- components/sections/entourage.tsx
+++ components/sections/entourage.tsx
@@ -940,7 +940,7 @@ export function Entourage() {
                       )}
 
                       {hasBestManOrMaid && (
-                        <TwoColumnLayout leftTitle="Best Men" rightTitle="Maid of Honor">
+                        <TwoColumnLayout leftTitle="Best Man" rightTitle="Maid of Honor">
                           {(() => {
                             const maxLen = Math.max(bestMan.length, maidOfHonor.length)
                             const rows = []
